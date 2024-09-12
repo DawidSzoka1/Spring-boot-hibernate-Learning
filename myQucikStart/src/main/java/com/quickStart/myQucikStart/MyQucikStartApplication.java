df449baaeda3d55791a17a22328b79989863e058
DawidSzoka1 <oficer130@gmail.com>
2024-09-12 17:52:49 +0200

    first spring boot project

---
+++ myQucikStart/src/main/java/com/quickStart/myQucikStart/MyQucikStartApplication.java
@@ -0,0 +1,13 @@
+package com.quickStart.myQucikStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQucikStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQucikStartApplication.class, args);
+	}
+
+}
---
+++ myQucikStart/src/test/java/com/quickStart/myQucikStart/MyQucikStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickStart.myQucikStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQucikStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
