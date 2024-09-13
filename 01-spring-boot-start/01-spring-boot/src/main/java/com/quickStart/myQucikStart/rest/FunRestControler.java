7e4d7165e75291e9a811b560f93f5ba74b4572e0
DawidSzoka1 <oficer130@gmail.com>
2024-09-13 19:19:24 +0200

    some small changes and new file with new webapp

--- 01-spring-boot-start/01-spring-boot/src/main/java/com/quickStart/myQucikStart/rest/FunRestControler.java
+++ 01-spring-boot-start/01-spring-boot/src/main/java/com/quickStart/myQucikStart/rest/FunRestControler.java
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestControler {
 
+    // inject properties for: coach.name, team.name
     @Value("${coach.name}")
     private String coachName;
 
-    @Value("${test}")
-    private int test;
+    @Value("${team.name}")
+    private String teamName;
 
-    @Value("${server.port}")
-    private int port;
-
-    @GetMapping("/{parem}/{one_more}/action/")
-    public String sayHello(@PathVariable String parem, @PathVariable String one_more) {
-        return "Hello world! " + coachName + " " + test + " port = " + port + " " + parem + one_more;
+    // endpoint for "coachName"
+    @GetMapping("/")
+    public String sayHello() {
+        return "say hello to " + coachName ;
     }
 
+    // endpoint for "teamName"
+    @GetMapping("/team/name")
+    public String sayTeamName() {
+        return teamName;
+    }
 }
