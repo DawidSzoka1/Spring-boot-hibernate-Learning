d1c4391770821f2170e171472857e1fc9b862175
DawidSzoka1 <oficer130@gmail.com>
2024-09-13 11:31:41 +0200

    using values from properties in RestController

--- myQucikStart/src/main/java/com/quickStart/myQucikStart/rest/FunRestControler.java
+++ myQucikStart/src/main/java/com/quickStart/myQucikStart/rest/FunRestControler.java
@@ -1,14 +1,24 @@
 package com.quickStart.myQucikStart.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestControler {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${test}")
+    private int test;
+
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello world!";
+        return "Hello world! " + coachName + " " + test + " port = " + port;
     }
 
 }
