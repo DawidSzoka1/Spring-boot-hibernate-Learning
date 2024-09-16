a92734e5ff3fc7d8557cbe9055005e0230066fbb
DawidSzoka1 <oficer130@gmail.com>
2024-09-16 19:36:14 +0200

    component scanning

---
+++ 02-spring-core/02-component-scanning/src/main/java/com/scanningcomponent/_02componentscanning/Application.java
@@ -0,0 +1,13 @@
+package com.scanningcomponent._02componentscanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
