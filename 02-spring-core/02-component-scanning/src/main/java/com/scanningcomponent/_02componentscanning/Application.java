e26d6a2b5f804d9516ecf36666a19caced160bba
DawidSzoka1 <oficer130@gmail.com>
2024-09-16 19:49:20 +0200

    added scanBasePackages

--- 02-spring-core/02-component-scanning/src/main/java/com/scanningcomponent/_02componentscanning/Application.java
+++ 02-spring-core/02-component-scanning/src/main/java/com/scanningcomponent/_02componentscanning/Application.java
@@ -3,7 +3,12 @@ package com.scanningcomponent._02componentscanning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.scanningcomponent._02componentscanning",
+                "utill"
+        }
+)
 public class Application {
 
     public static void main(String[] args) {
