e49e33776fe45e2c74cc2c727aa4b7a502429271
DawidSzoka1 <oficer130@gmail.com>
2024-09-19 16:31:59 +0200

    sql for some database and crud application init

---
+++ 03-hibernate-jpa-crud/01-cruddemo/src/main/java/com/hibernatejpa/cruddemo/CruddemoApplication.java
@@ -0,0 +1,21 @@
+package com.hibernatejpa.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return _ -> {
+            System.out.println("Hello World");
+        };
+    }
+}
