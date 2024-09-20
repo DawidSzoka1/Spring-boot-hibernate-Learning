946034daf106b0c142c8ad6423d1ac152d1cb637
DawidSzoka1 <oficer130@gmail.com>
2024-09-20 16:38:13 +0200

    get all students and find by name

--- 03-hibernate-jpa-crud/01-cruddemo/src/main/java/com/hibernatejpa/cruddemo/dao/StudentDAO.java
+++ 03-hibernate-jpa-crud/01-cruddemo/src/main/java/com/hibernatejpa/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.hibernatejpa.cruddemo.dao;
 
 import com.hibernatejpa.cruddemo.entity.Student;
 
+import java.util.List;
+
 
 public interface StudentDAO {
     void save(Student student);
     Student getStudentById(int id);
+    List<Student> getAllStudents();
+    List<Student> getAllStudentsByName(String firstName, String lastName);
 }
--- 03-hibernate-jpa-crud/01-cruddemo/src/main/java/com/hibernatejpa/cruddemo/daoimp/StudentDAOImpl.java
+++ 03-hibernate-jpa-crud/01-cruddemo/src/main/java/com/hibernatejpa/cruddemo/daoimp/StudentDAOImpl.java
@@ -3,9 +3,12 @@ package com.hibernatejpa.cruddemo.daoimp;
 import com.hibernatejpa.cruddemo.dao.StudentDAO;
 import com.hibernatejpa.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 @Repository//sub class of Component
@@ -27,5 +30,21 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student getStudentById(int id) {
         return entityManager.find(Student.class, id);//if not find return null
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return entityManager.createQuery("FROM Student", Student.class).getResultList();
+    }
+
+    @Override
+    public List<Student> getAllStudentsByName(String firstName, String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery(
+                "FROM Student WHERE firstName like :first and lastName like :last",
+                Student.class
+        );
+        query.setParameter("first", firstName).setParameter("last", lastName);
+
+        return query.getResultList();
     }
 }
