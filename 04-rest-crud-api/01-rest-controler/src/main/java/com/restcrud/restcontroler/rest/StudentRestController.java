91f629cc7fa37a4e4d912c01302be4439f1fe98c
DawidSzoka1 <oficer130@gmail.com>
2024-09-22 20:44:05 +0200

    some more testing with returning json

--- 04-rest-crud-api/01-rest-controler/src/main/java/com/restcrud/restcontroler/rest/StudentRestController.java
+++ 04-rest-crud-api/01-rest-controler/src/main/java/com/restcrud/restcontroler/rest/StudentRestController.java
@@ -1,9 +1,7 @@
 package com.restcrud.restcontroler.rest;
 
 import com.restcrud.restcontroler.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +12,32 @@ public class StudentRestController {
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<Student>();
+        List<Student> students = new ArrayList<>();
 
         students.add(new Student("first", "last"));
         students.add(new Student("Mario", "Ball"));
         students.add(new Student("first", "leo"));
+        //spring will use jackson to convert it to json using getters from student class
         return students;
+    }
+
+    @GetMapping("/student/{name}")
+    public List<Student> getStudentsByName(
+            // type ?firstName=value after path
+//            @RequestParam("firstName") String name
+            @PathVariable String name
+    ) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("first", "last"));
+        students.add(new Student("Mario", "Ball"));
+        students.add(new Student("first", "leo"));
+        List<Student> namein = new ArrayList<>();
+        for(Student student : students) {
+            if(student.getFirstName().equals(name)) {
+                namein.add(student);
+            }
+        }
+
+        return namein;
     }
 }
