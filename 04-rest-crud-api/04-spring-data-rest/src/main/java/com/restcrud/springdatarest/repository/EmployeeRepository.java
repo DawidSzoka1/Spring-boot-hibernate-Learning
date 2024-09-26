777820dfc5e853bbde3ab73e5737683eb3623948
DawidSzoka1 <oficer130@gmail.com>
2024-09-26 14:26:17 +0200

    some properties for data rest

--- 04-rest-crud-api/04-spring-data-rest/src/main/java/com/restcrud/springdatarest/repository/EmployeeRepository.java
+++ 04-rest-crud-api/04-spring-data-rest/src/main/java/com/restcrud/springdatarest/repository/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.restcrud.springdatarest.repository;
 
 import com.restcrud.springdatarest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//change path
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
