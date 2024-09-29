604b6b798fa4e7b0b34773ea126ed0b84ecdb94e
DawidSzoka1 <oficer130@gmail.com>
2024-09-29 18:55:51 +0200

    another way of getting values from thymeleaf

--- 06-spring-mvc-thymeleaf/01-thymeleaf-demo/src/main/java/com/springmvcthymealeaf/thymealeafdemo/controller/HelloWorldController.java
+++ 06-spring-mvc-thymeleaf/01-thymeleaf-demo/src/main/java/com/springmvcthymealeaf/thymealeafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -16,12 +17,18 @@ public class HelloWorldController {
 
 
     @RequestMapping("/processFromVersionTwo")
-    public String letsShoutDude(HttpServletRequest request, Model model){
-        String name = "Yo! " + request.getParameter("studentName").toUpperCase();
-        model.addAttribute("studentName", name);
-
+    public String processFromVersionTwo(@RequestParam("studentName") String name, Model model){
+        model.addAttribute("name", name.toUpperCase());
         return "helloworld2";
     }
+
+//    @RequestMapping("/processFromVersionTwo")
+//    public String letsShoutDude(HttpServletRequest request, Model model){
+//        String name = "Yo! " + request.getParameter("studentName").toUpperCase();
+//        model.addAttribute("studentName", name);
+//
+//        return "helloworld2";
+//    }
 
     @RequestMapping("/processForm")
     public String processForm(){
