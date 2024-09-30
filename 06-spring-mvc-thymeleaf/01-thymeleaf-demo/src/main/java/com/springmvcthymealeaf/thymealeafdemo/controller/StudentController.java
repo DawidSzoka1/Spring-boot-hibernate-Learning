40723b33c1a67e66a0eff8e56b4dabca5fb56911
DawidSzoka1 <oficer130@gmail.com>
2024-09-30 16:30:20 +0200

    loop in thymeleaf

--- 06-spring-mvc-thymeleaf/01-thymeleaf-demo/src/main/java/com/springmvcthymealeaf/thymealeafdemo/controller/StudentController.java
+++ 06-spring-mvc-thymeleaf/01-thymeleaf-demo/src/main/java/com/springmvcthymealeaf/thymealeafdemo/controller/StudentController.java
@@ -1,19 +1,27 @@
 package com.springmvcthymealeaf.thymealeafdemo.controller;
 
 import com.springmvcthymealeaf.thymealeafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/student")
     public String student(Model model) {
         Student student = new Student("test", "test", "India");
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
