5aa6c239d0018a211c1ea3b2b2342bcf8a97b81e
DawidSzoka1 <oficer130@gmail.com>
2024-10-28 20:00:36 +0100

    added dependency plus some basic mapping

--- SmallProjects/FullUserSite/src/main/java/com/example/fullusersite/User/UserController.java
+++ SmallProjects/FullUserSite/src/main/java/com/example/fullusersite/User/UserController.java
@@ -1,32 +1,44 @@
 package com.example.fullusersite.User;
 
-
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
 public class UserController {
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "login-form";
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User user){
+    public String login(@Valid @ModelAttribute("user") User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "login-form";
+        }
         return "login-post";
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "register-form";
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody User user){
+    public String register(@Valid @ModelAttribute("user") User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "register-form";
+        }
         return "register-post";
     }
 
