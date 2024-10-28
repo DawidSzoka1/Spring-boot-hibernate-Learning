4563d6f4e5489527e014991fa2886b73b7fc334e
DawidSzoka1 <oficer130@gmail.com>
2024-10-28 19:44:12 +0100

    added dependency plus html views added

--- SmallProjects/FullUserSite/src/main/java/com/example/fullusersite/User/UserController.java
+++ SmallProjects/FullUserSite/src/main/java/com/example/fullusersite/User/UserController.java
@@ -1,4 +1,33 @@
 package com.example.fullusersite.User;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
 public class UserController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login-form";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        return "login-post";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register-form";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user){
+        return "register-post";
+    }
+
 }
