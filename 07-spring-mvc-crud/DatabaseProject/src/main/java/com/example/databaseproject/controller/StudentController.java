fbf1bd18ae9cf5ccd3c31b7644ba4119469388cb
DawidSzoka1 <oficer130@gmail.com>
2024-10-31 15:08:04 +0100

    adding student

--- 07-spring-mvc-crud/DatabaseProject/src/main/java/com/example/databaseproject/controller/StudentController.java
+++ 07-spring-mvc-crud/DatabaseProject/src/main/java/com/example/databaseproject/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.example.databaseproject.controller;
 
+import com.example.databaseproject.entity.Student;
 import com.example.databaseproject.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,4 +27,18 @@ public class StudentController {
         return "list-students";
     }
 
+    @GetMapping("/formAdd")
+    public String showForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "form-student";
+    }
+
+    @PostMapping("/save")
+    public String postForm(@Valid Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "form-student";
+        }
+        studentService.saveStudent(student);
+        return "redirect:/student/list";
+    }
 }
