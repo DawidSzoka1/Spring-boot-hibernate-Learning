0b2571c5fb01f21611e33b7485b3da53b83d17b7
DawidSzoka1 <oficer130@gmail.com>
2024-11-01 16:16:38 +0100

    added some new users

---
+++ 08-spring-mvc-security/01-custom-users-authentication/src/main/java/com/example/customusersauthentication/security/DemoSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.customusersauthentication.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")//{noop} means plain text
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
